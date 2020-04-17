4502e48accb6712077fbf3fa0f65c41f7075197d
Channing_Wang <a0911774228@gmail.com>
2020-04-17 15:32:51 +0800

    3.sql 更新 champion_name

--- 3.sql
+++ 3.sql
@@ -1,6 +1,6 @@
-select champion_id, count(*) as cnt
-from participant
-where position = 'JUNGLE'
+select c.champion_name, count(*) as cnt
+from participant as p, champ as c
+where c.champion_id = p. champion_id and p.position = 'JUNGLE'
 group by champion_id
 order by cnt desc
 limit 3;
