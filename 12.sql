611e8ef207d14fd045d048cc1964ee02235466cc
Channing-Wang <a0911774228@gmail.com>
2020-04-22 12:59:23 +0800

    update 12.sql

--- 12.sql
+++ 12.sql
@@ -1,4 +1,4 @@
-select adc2.champion_name as ADC, support2.champion_name as SUPPORT, sum(adc2.win)/count(adc2.win) as win_ratio, count(*) as battles
+select adc2.champion_name as ADC, support2.champion_name as SUPPORT, sum(adc2.win)/count(adc2.win) as win_ratio, avg(support2.assists) as support_avg_Assists, count(*) as battles
 from
 (select adc.match_id, adc.champion_name, adc.team, s.win
 from
@@ -6,7 +6,7 @@ from
 from participant as p, champ as c
 where p.champion_id = c.champion_id and p.position = 'DUO_CARRY') as adc, stat as s
 where adc.player_id = s.player_id) as adc2,
-(select support.match_id, support.champion_name, support.team
+(select support.match_id, support.champion_name, support.team, s.assists
 from
 (select p.match_id, p.player_id, c.champion_name, if(p.player > 5, 'R', 'B') as team
 from participant as p, champ as c
@@ -17,3 +17,17 @@ group by ADC, SUPPORT
 having battles > 1000
 order by win_ratio desc
 limit 5;
+
+select avg(ps.assists) as support_avg_Assists
+from
+(
+select p.champion_id, s.win, s.assists
+from participant as p, stat as s
+where p.player_id = s.player_id and s.win = 1 and p.position = 'DUO_SUPPORT'
+)as ps
+group by ps.champion_id
+having count(*) > 1000
+order by support_avg_Assists desc
+limit 5;
+
+
